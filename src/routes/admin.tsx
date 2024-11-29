4284c5bddffe1e81c6a22e1e2d93268f25c7f43c
Jonatan Helander <jonatanhelander@hotmail.com>
2024-11-29 13:05:02 +0100

    Added admin page and updated outdated package

---
+++ src/routes/admin.tsx
@@ -0,0 +1,9 @@
+import Header from "~/components/Header";
+
+export default function Admin() {
+  return (
+    <main class="flex min-h-screen flex-col bg-black">
+      <Header />
+    </main>
+  );
+}
