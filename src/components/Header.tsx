0ea535382d130e5c626191f88e3f469daf9c19fb
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-12-17 13:07:04 +0100

    Added footer component, icon library, and links to social media

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div
       id="gradient-border"
-      class="bg-gradient-to-r from-[#1b1b1b] from-1% via-blue-500 via-50% to-[#1b1b1b] to-99% pb-[2px] sticky top-0"
+      class="bg-gradient-to-r from-[#1b1b1b] from-1% via-blue-500 via-50% to-[#1b1b1b] to-99% pb-[2px] sticky top-0 z-50"
     >
       <header class="background-black flex bg-[#1b1b1b]">
         <a href="/" class="flex items-center px-12  border-bottom-effect p-4">
