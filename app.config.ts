e080e4eac105510abc61784e53a3c3a56251fed8
Jonatan Helander <Jonatanhelander@hotmail.com>
2025-01-02 14:52:07 +0100

    built project

--- app.config.ts
+++ app.config.ts
@@ -6,15 +6,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Get the repository name for GitHub Pages
-const BASE_URL = "/Sorting-Algorithm-Visualization"; // Adjust this to your repo name
-
 export default defineConfig({
   ssr: true,
-  server: {
-    baseURL: BASE_URL,
-    preset: "github-pages",
-  },
   vite: {
     resolve: {
       alias: {
