10bece898be1b684c4e503742fee677e771b5b89
Jonatan Helander <Jonatanhelander@hotmail.com>
2025-01-04 16:42:03 +0100

    Seeding of another admin

--- prisma/seed/users.ts
+++ prisma/seed/users.ts
@@ -5,6 +5,9 @@ export async function seedAdmins() {
   const name = process.env.OWNER_NAME;
   const image = process.env.OWNER_IMAGE;
 
+  const examinatorEmail = process.env.EXAMINATOR_IDENTIFIER;
+  const examinatorName = process.env.EXAMINATOR_NAME;
+
   await db.user.upsert({
     where: { email: email },
     update: {},
@@ -13,6 +16,17 @@ export async function seedAdmins() {
       email: email,
       isAdmin: true,
       image: image,
+    },
+  });
+
+  await db.user.upsert({
+    where: { email: examinatorEmail },
+    update: {},
+    create: {
+      name: examinatorName,
+      email: examinatorEmail,
+      isAdmin: true,
+      image: "",
     },
   });
 }
